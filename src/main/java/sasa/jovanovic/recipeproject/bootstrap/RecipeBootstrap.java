1b96340048758283fdabc1c2a9d2c558b510534b
sjovanovic974 <sjovanovic974@gmail.com>
2021-11-10 07:54:25 +0100

    Ading transactional

--- src/main/java/sasa/jovanovic/recipeproject/bootstrap/RecipeBootstrap.java
+++ src/main/java/sasa/jovanovic/recipeproject/bootstrap/RecipeBootstrap.java
@@ -1,8 +1,10 @@
 package sasa.jovanovic.recipeproject.bootstrap;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import sasa.jovanovic.recipeproject.domain.*;
 import sasa.jovanovic.recipeproject.repositories.CategoryRepository;
 import sasa.jovanovic.recipeproject.repositories.RecipeRepository;
@@ -13,6 +15,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -27,8 +30,10 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loading Bootstrap Data");
     }
 
     private List<Recipe> getRecipes() {
--- src/main/java/sasa/jovanovic/recipeproject/controllers/IndexController.java
+++ src/main/java/sasa/jovanovic/recipeproject/controllers/IndexController.java
@@ -1,16 +1,12 @@
 package sasa.jovanovic.recipeproject.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sasa.jovanovic.recipeproject.domain.Category;
-import sasa.jovanovic.recipeproject.domain.UnitOfMeasure;
-import sasa.jovanovic.recipeproject.repositories.CategoryRepository;
-import sasa.jovanovic.recipeproject.repositories.UnitOfMeasureRepository;
 import sasa.jovanovic.recipeproject.services.RecipeService;
 
-import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -23,6 +19,7 @@ public class IndexController {
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model){
 
+        log.debug("Getting Index Page");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
